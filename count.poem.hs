854b0117aa1b9a61141a4177769cea90782eef18
benibela <benito@benibela.de>
2013-03-04 02:14:10 +0100

    correct Homespring
    --HG--
    branch : trunks

--- count.poem.hs
+++ count.poem.hs
@@ -1,6 +1,6 @@
-Universe of fish swimming home in spring. carefully, carefully there comes an upstream. killing. device
+Universe  of fish swimming home in spring, carefully, carefully there comes an upstream. killing. device
 young. switch young fish spring. young fish spring around and around. forward, backward, always around
-young fish spring, spring around. see the water, see the river. Carefully carefully again, now comes an bear bridge
+young fish spring, spring around. see the water, see the river. Carefully carefully again, now comes a bear bridge
 See. a hatchery Powers 9.
    hatchery Powers 100.
    marshy marshy marshy snowmelt                                   insulated append. down
@@ -14,8 +14,22 @@ Now it is safe! Admire the birthplaces hatchery Powers 9.
      swimming around hatchery Powers 5.
      Oh no! hatchery Powers 6.
      Peace's over hatchery Powers 7.
-     Watch out hatchery Powers 8.
-                     DANGER! upstream. killing. device young. switch Did you make it? That was only the first. Many will come, many will die
-The bears are warning signs. Danger follows them. But not directly. ATTENTION! bear hatchery Powers 9                             insulated upstream. killing. device young. switch The next killing device. a b
-Who died? So many, so sad. But that is the way of life. Watch out the next one comes! bear hatchery Powers 8                          insulated upstream. killing. device young. switch Some went lost again, some made it, but always less. How did we sin? c a b c bear hatchery Powers 7                      
-insulated upstream. killing. device young. switch j a b c d e f g h i j a b c bear hatchery Powers 6                    insulated upstream. killing. device young. switch Soon it is over! Half way we are through! Beware a bear hatchery Powers 5                 insulated upstream. killing. device young. switch j a b c d e f g bear hatchery Powers 4              insulated upstream. killing. device young. switch j a b c d bear hatchery Powers 3           insulated upstream. killing. device young. switch the end bear hatchery Powers 2        finally insulated bear hatchery Powers 1
+     Watch out! hatchery Powers 8.
+                     DANGER! upstream. killing. device young. switch
+Did you make it? That was only the first. Many will come, many will die
+The bears are warning signs. Danger follows them. But not directly. ATTENTION!
+bear hatchery Powers 9                             insulated upstream. killing. device
+young. switch The next killing device. Who died? So many, so sad. But that is the way of our life. Watch out! The next one!
+bear hatchery Powers 8                          insulated upstream. killing. device young. switch
+Some went lost again, some made it, but always less. It is getting more and more dangerous. Carefully here! bear hatchery Powers 7                       insulated upstream. killing. device young. switch
+That went well. Less and less, lifetime we have. There. just ten more killing devices to cross
+bear hatchery Powers 6                    insulated upstream. killing. device young. switch
+Soon it is over! Halfway we are through! Beware yet another bear hatchery Powers 5                 insulated upstream. killing. device young. switch
+Arrgh!!!. Don't worry he went to a better place/river bear hatchery Powers 4             
+insulated upstream. killing. device young. switch
+Such is the fish's life bear hatchery Powers 3          
+insulated upstream. killing. device young. switch
+The End!
+bear hatchery Powers 2       
+Finally!
+insulated bear hatchery Powers 1
