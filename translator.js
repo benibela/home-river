f481db7a9bd78ba3ca434119835bc8529178224f
benibela <benito@benibela.de>
2013-03-05 14:55:18 +0100

    homespring brackets + comments
    --HG--
    branch : trunks

--- translator.js
+++ translator.js
@@ -1,54 +1,77 @@
 %SCRIPT
-var indentationLevel = 0;
 var tokenCount = 0;
 var indentations = [-1,0];
-var indentationTokens = {"0": 0, "-1": 0};
+var tokenCounts = [0, 0];
+var bracketProtected = [0, 0];
+
 var lines = editor.document().textLines();
 var result = "";
 var previousTokenCount = 0;
 for (var li=0;li<lines.length;li++) {
 	var l = lines[li];
-	if (l.trim() == "") continue;
-	var appendToLastToken =  /[^.][.][>][*]/.test(l);
-	if ( appendToLastToken ) 
-	  l.remove(".>*")
-	 
+	if (l.trim() == "") continue; 
 	//if (l == "") break;
 	var tokenizer = /^( *)(([.] |[^ ])*)(.*)$/;
 	var token = tokenizer.exec(l);
+// alert(l+"\n"+tokenCount+" "+token+"\n"+indentations+"\n"+tokenCounts+"\n"+bracketProtected)
+	function popAllLast(){
+		for (var i = tokenCounts[indentations.length-1]; i > tokenCounts[indentations.length-2]; i--)
+			result += " ";
+		indentations.pop();
+		tokenCounts.pop();
+		tokenCount = tokenCounts[tokenCounts.length-1];
+		bracketProtected.pop();
+	}
+
+	if (token[2] == ".}") {
+		while (token[2] != "") {
+			if (token[2] == ".}") 
+				while (!bracketProtected[bracketProtected.length-1]) popAllLast();
+			popAllLast();
+			token = tokenizer.exec(token[4]);
+		}
+		continue;
+	} else if (token[2] == ".%") continue;
 
 	var newIndentationLevel = token[1].length;
-	if ( !appendToLastToken ) {
-	  for (var i=0;i<previousTokenCount-1;i++) result += " ";
-	  tokenCount -= previousTokenCount - 1;
-	}
-	previousTokenCount = 0;
-
 	if (newIndentationLevel > indentations[indentations.length-1]) {
 		indentations.push(newIndentationLevel);
-		indentationTokens[newIndentationLevel] = tokenCount;	
+		tokenCounts.push(tokenCount);
+		bracketProtected.push(false);
+	} else if (result != "" && newIndentationLevel == indentations[indentations.length-1]) {
+	  result += " ";
+	  tokenCount--;
 	} else { 
-		while (newIndentationLevel < indentations[indentations.length-1]) {
-		  for (var i = indentationTokens[indentations[indentations.length-1]]; 
-		           i > indentationTokens[indentations[indentations.length-2]]; i--)
-				result += " ";
-			indentations.pop();
-			tokenCount = indentationTokens[indentations[indentations.length-1]];
+		while (newIndentationLevel < indentations[indentations.length-1] && !bracketProtected[bracketProtected.length-1]) {
+			popAllLast();
 		}
-  	if (result != "" && newIndentationLevel == indentations[indentations.length-1]) result += " ";
+ 	  if (result != "" && newIndentationLevel == indentations[indentations.length-1]) result += " ";
  	}
-	if (token[2] != "") { result += " "+token[2]; previousTokenCount++; }
-
+ 	var newTokenCount = 0;
+	if (token[2] != "") { result += " "+token[2]; newTokenCount++; }
+  var protectionCount = 1;
 	while (token[2] != "") {
 		newIndentationLevel += token[2].length;
 		//alert(token)	
 		token = tokenizer.exec(token[4]);
 		newIndentationLevel += token[1].length;
-		if (token[2] != "") { result += " "+ token[2]; previousTokenCount++; }
+		if (token[2] != "") {
+			if (token[2] == ".{") { 
+				protectionCount = newTokenCount;
+				indentations.push(0);
+				tokenCounts.push(tokenCount+newTokenCount);
+				bracketProtected.push(true);
+			} else if (token[2] == ".%") { break; }
+			else { result += " "+ token[2]; newTokenCount++; }
+		}
 	//if (!confirm(token)) return;
 	
 	}
-	tokenCount += previousTokenCount;
+	if (newTokenCount > 0) {
+	  var temp = newTokenCount;
+		for (var i=protectionCount;i<temp;i++) { result += " "; newTokenCount-=1;}
+		tokenCount += newTokenCount;
+	}
 	//alert(newIndentationLevel+token[1]+":"+indentations);
 
 }
@@ -57,9 +80,9 @@ for (var li=0;li<lines.length;li++) {
 result = result.trim();
 result += "\n";
 result = result.replace(/[.]n /g, ".\n");
-alert(result.replace(/ /g, " _ "));
+//alert(result.replace(/ /g, " _ "));
 
-
+///*
 writeFile("/tmp/test.hs", result)
 system("txs:///homespring /tmp/test.hs | tail -n 100 | column > txs:///messages");
 //*/
